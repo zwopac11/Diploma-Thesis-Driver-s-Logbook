74cc90bfad1082999b2a135cbfd4f4b45c7d5b32
zwopac11 <zwopac11@htlkaindorf.at>
2016-02-26 20:28:47 +0100

    “competitoranalysis_add_table”

--- clean_build.bat
+++ clean_build.bat
@@ -2,11 +2,14 @@ del Alt
 move *.aux Alt
 move *.bbl Alt
 move *.bcf Alt
+move *.blg Alt
 move *.glg Alt
 move *.glo Alt
 move *.gls Alt
 move *.ist Alt
 move *.log Alt
 move *.out Alt
+move *.pdf Alt
+move *.run.xml Alt
 move *.synctex.gz Alt
 move *.toc Alt
