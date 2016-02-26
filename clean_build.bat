fff0c50db423899be166df0f1712c6675c8d7069
zwopac11 <zwopac11@htlkaindorf.at>
2016-02-26 19:19:01 +0100

    “bilder_ordner_competitor_analysis”

---
+++ clean_build.bat
@@ -0,0 +1,12 @@
+del Alt
+move *.aux Alt
+move *.bbl Alt
+move *.bcf Alt
+move *.glg Alt
+move *.glo Alt
+move *.gls Alt
+move *.ist Alt
+move *.log Alt
+move *.out Alt
+move *.synctex.gz Alt
+move *.toc Alt
